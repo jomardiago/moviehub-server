1690c207f14e352fed7170a46e36c59bb23764f1
Jose <Jose.Diago@kyndryl.com>
2022-02-08 20:42:58 +0800

    Initial Commit

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    Query: {
+        sayHello() {
+            return 'Hello Jose Diago';
+        }
+    }
+}
---
+++ server.js
@@ -0,0 +1,13 @@
+const { ApolloServer } = require('apollo-server');
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen({ port: 5000 }).then(res => {
+    console.log(`Server running at url ${res.url}`);
+});
